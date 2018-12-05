01e4a9dc57cebc34c047013d642f4f355796e26f
huuchi207 <huuchi207@gmail.com>
2018-12-05 17:50:43 +0700

    exit on close

--- src/br/com/museuid/app/Login.java
+++ src/br/com/museuid/app/Login.java
@@ -3,6 +3,8 @@ package br.com.museuid.app;
 import br.com.museuid.util.BundleUtils;
 import br.com.museuid.util.Messenger;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
@@ -10,6 +12,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * Inicialização tela de login
@@ -18,52 +21,58 @@ import javafx.stage.Stage;
  */
 public class Login extends Application {
 
-    public static Stage mStage;
-    private Scene mScene;
-    private AnchorPane rootPane;
+  public static Stage mStage;
+  private Scene mScene;
+  private AnchorPane rootPane;
 
-    private Screen screen = Screen.getPrimary();
-    private Rectangle2D windows = screen.getVisualBounds();
+  private Screen screen = Screen.getPrimary();
+  private Rectangle2D windows = screen.getVisualBounds();
 
-    @Override
-    public void start(final Stage stage) {
-        try {
-            mStage = stage;
+  @Override
+  public void start(final Stage stage) {
+    try {
+      mStage = stage;
 
-            FXMLLoader fxml = new FXMLLoader(App.class.getResource("/br/com/museuid/screen/login/login.fxml"));
-            fxml.setResources(BundleUtils.getResourceBundle());
-            rootPane = fxml.load();
+      FXMLLoader fxml = new FXMLLoader(App.class.getResource("/br/com/museuid/screen/login/login.fxml"));
+      fxml.setResources(BundleUtils.getResourceBundle());
+      rootPane = fxml.load();
 
-            mScene = new Scene(rootPane);
+      mScene = new Scene(rootPane);
 
 //            stage.setX(windows.getMinX());
 //            stage.setY(windows.getMinY());
 //            stage.setWidth(619);
 //            stage.setHeight(445);
-            stage.resizableProperty().setValue(Boolean.FALSE);
+      stage.resizableProperty().setValue(Boolean.FALSE);
+      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                                @Override
+                                public void handle(WindowEvent event) {
+                                  Platform.exit();
+                                  System.exit(0);
+                                }
+                              });
 //            stage.setMinWidth(windows.getWidth()-200);
 //            stage.setMinHeight(windows.getHeight()-100);
-            stage.centerOnScreen();
+        stage.centerOnScreen();
 
 
+      stage.getIcons().addAll(new Image(Login.class.getResourceAsStream("icon.png")));
 
-            stage.getIcons().addAll(new Image(Login.class.getResourceAsStream("icon.png")));
-
-            stage.setScene(mScene);
+      stage.setScene(mScene);
 //            ResizeHelper.addResizeListener/(stage);
-            stage.show();
+      stage.show();
 
-        } catch (Exception ex) {
-            Messenger.erro(BundleUtils.getResourceBundle().getString("txt_error") + ex);
-        }
+    } catch (Exception ex) {
+      Messenger.erro(BundleUtils.getResourceBundle().getString("txt_error") + ex);
     }
+  }
 
-    public static void main(String[] args) {
-        Application.launch(Login.class, (java.lang.String[]) null);
-    }
+  public static void main(String[] args) {
+    Application.launch(Login.class, (java.lang.String[]) null);
+  }
 
-    public static Stage getmStage(){
-        return mStage;
-    }
+  public static Stage getmStage() {
+    return mStage;
+  }
 
 }
