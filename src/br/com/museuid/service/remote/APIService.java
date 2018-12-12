ab59258d59d3950d2e96cdf5847d93511ac72b3b
huuchi207 <huuchi207@gmail.com>
2018-12-12 09:46:20 +0700

    change api links

--- src/br/com/museuid/service/remote/APIService.java
+++ src/br/com/museuid/service/remote/APIService.java
@@ -1,156 +1,137 @@
 package br.com.museuid.service.remote;
 
-import java.util.List;
-
-import br.com.museuid.dto.ChartData;
-import br.com.museuid.dto.DeviceInfo;
-import br.com.museuid.dto.Product;
-import br.com.museuid.dto.SecureQuestionDTO;
-import br.com.museuid.dto.SessionDeviceInfo;
-import br.com.museuid.dto.SessionUserInfo;
-import br.com.museuid.dto.UploadImageDTO;
-import br.com.museuid.dto.UserDTO;
+import br.com.museuid.dto.*;
 import br.com.museuid.dto.sample.Item;
 import br.com.museuid.model.data.OrderDetail;
-import br.com.museuid.service.remote.requestbody.AddDeviceRequest;
-import br.com.museuid.service.remote.requestbody.ChangePasswordRequest;
-import br.com.museuid.service.remote.requestbody.LoginRequest;
-import br.com.museuid.service.remote.requestbody.SecureQuestionRequest;
-import br.com.museuid.service.remote.requestbody.StatisticRequest;
-import br.com.museuid.service.remote.requestbody.StockImportingRequest;
+import br.com.museuid.service.remote.requestbody.*;
 import br.com.museuid.service.remote.sample.SampleResponseDTO;
 import okhttp3.MultipartBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.DELETE;
-import retrofit2.http.GET;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
-import retrofit2.http.Part;
-import retrofit2.http.Query;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface APIService {
   //Sample
   @GET("data/2.5/box/city")
   Call<SampleResponseDTO<Item>> getSample(@Query("bbox") String bbox, @Query("appid") String appid);
 
-  @PUT("client-admin")
+  @PUT("clients/client-admin")
   Call<ResponseDTO<DeviceInfo>> addDevice(@Body AddDeviceRequest addDeviceRequest);
 
-  @GET("auto-client")
+  @GET("clients/auto-client")
   Call<ResponseDTO<SessionDeviceInfo>> checkDevice(@Query("macaddress") String macAddress);
 
-  @GET("clients")
+  @GET("clients/clients")
   Call<ResponseDTO<String>> getClients();
 
-  @GET("products")
+  @GET("products/products")
   Call<ResponseDTO<List<Product>>> getProductList();
 
-  @POST("login")
+  @POST("users/login")
   Call<ResponseDTO<SessionUserInfo>> login(@Body LoginRequest loginRequest);
 
-  @PUT("product")
+  @PUT("products/product")
   Call<ResponseDTO<Product>> addProduct(@Body Product product);
 
-  @POST("product")
+  @POST("products/product")
   Call<ResponseDTO<Product>> updateProduct(@Body Product product);
 
-  @DELETE("product")
+  @DELETE("products/product")
   Call<ResponseDTO<Object>> deleteProduct(@Query("productid") String productId);
 
-  @PUT("queue")
+  @PUT("queues/queue")
   Call<ResponseDTO<Object>> putOrder(@Body OrderDetail putQueueRequest);
 
-  @GET("logout")
+  @GET("users/logout")
   Call<ResponseDTO<Object>> logOut();
 
-  @GET("user")
+  @GET("users/user")
   Call<ResponseDTO<UserDTO>> getCurrentUserInfo(@Query("userid") String userId);
 
-  @POST("user")
+  @POST("users/user")
   Call<ResponseDTO<UserDTO>> changeUserInfo(@Body UserDTO userDTO);
 
-  @POST("change-password")
+  @POST("users/change-password")
   Call<ResponseDTO<Object>> changePassword(@Body ChangePasswordRequest request);
 
-  @GET("users")
+  @GET("users/users")
   Call<ResponseDTO<List<UserDTO>>> getListUser();
 
-  @DELETE("user")
+  @DELETE("users/user")
   Call<ResponseDTO<Object>> deleteUser(@Query("userid") String userId);
 
-  @PUT("user")
+  @PUT("users/user")
   Call<ResponseDTO<UserDTO>> addUser(@Body UserDTO userDTO);
 
-  @POST("reset-password")
+  @POST("users/reset-password")
   Call<ResponseDTO<UserDTO>> resetPasswordForCustomer(@Body UserDTO userDTO);
 
-  @POST("client")
+  @POST("clients/client")
   Call<ResponseDTO<Object>> updateDeviceInfo(@Body DeviceInfo product);
 
-  @DELETE("client")
+  @DELETE("clients/client")
   Call<ResponseDTO<Object>> deleteDevice(@Query("clientid") String clientId);
 
-  @GET("clients")
+  @GET("clients/clients")
   Call<ResponseDTO<List<DeviceInfo>>> getListDevice();
 
-  @POST("queue")
+  @POST("queues/queue")
   Call<ResponseDTO<Object>> updateQueue(@Body OrderDetail orderDetail);
 
-  @POST("days-period")
+  @POST("statistics/days-period")
   Call<ResponseDTO<ChartData>> getDayStatisticPeriod(@Body StatisticRequest statisticRequest);
 
-  @POST("months-period")
+  @POST("statistics/months-period")
   Call<ResponseDTO<ChartData>> getMonthStatisticPeriod(@Body StatisticRequest statisticRequest);
 
-  @POST("years-period")
+  @POST("statistic/years-period")
   Call<ResponseDTO<ChartData>> getYearStatisticPeriod(@Body StatisticRequest statisticRequest);
 
-  @GET("queues")
+  @GET("queues/queues")
   Call<ResponseDTO<List<OrderDetail>>> getOrderByStatus(@Query("filter") String statusFilter, @Query("handlerid") String handlerid);
 
-  @POST("days")
+  @POST("statistic/days")
   Call<ResponseDTO<ChartData>> getDayStatistic(@Body StatisticRequest statisticRequest);
 
-  @POST("months")
+  @POST("statistic/months")
   Call<ResponseDTO<ChartData>> getMonthStatistic(@Body StatisticRequest statisticRequest);
 
-  @POST("years")
+  @POST("statistic/years")
   Call<ResponseDTO<ChartData>> getYearStatistic(@Body StatisticRequest statisticRequest);
 
-  @POST("line/revenue")
+  @POST("statistic/line/revenue")
   Call<ResponseDTO<ChartData>> getProductRevenueStatistic(@Body StatisticRequest statisticRequest);
 
-  @POST("line/sales")
+  @POST("statistic/line/sales")
   Call<ResponseDTO<ChartData>> getProductSalesStatistic(@Body StatisticRequest statisticRequest);
 
-  @POST("request")
+  @POST("pendingproducts/request")
   Call<ResponseDTO<Object>> updateImportingStockRequest(@Body StockImportingRequest stockImportingRequest);
 
-  @PUT("request")
+  @PUT("pendingproducts/request")
   Call<ResponseDTO<Object>> createImportingStockRequest(@Body StockImportingRequest stockImportingRequest);
 
-  @DELETE("request")
+  @DELETE("pendingproducts/request")
   Call<ResponseDTO<Object>> deleteImportingStockRequest(@Query("pendingproductid") String pendingproductid);
 
-  @GET("requests")
+  @GET("pendingproducts/requests")
   Call<ResponseDTO<List<StockImportingRequest>>> getImportingStockRequestList(@Query("filter") String filter, @Query("requesterid") String requesterid);
 
-  @POST("request/decision")
+  @POST("pendingproducts/request/decision")
   Call<ResponseDTO<Object>> handleImportingRequest(@Body StockImportingRequest request);
 
-  @GET("secure-question")
+  @GET("users/secure-question")
   Call<ResponseDTO<SecureQuestionDTO>> getListSecureQuestion();
 
-  @POST("update-secure")
+  @POST("users/update-secure")
   Call<ResponseDTO<Object>> updateSecureQuestion(@Body SecureQuestionRequest secureQuestionRequest);
 
 
-  @POST("forgot-password")
+  @POST("users/forgot-password")
   Call<ResponseDTO<UserDTO>> forgotPass(@Body SecureQuestionRequest secureQuestionRequest);
 
   @Multipart
-  @POST("image")
+  @POST("images/image")
   Call<ResponseDTO<UploadImageDTO>> uploadImage(@Part MultipartBody.Part image);
 }
